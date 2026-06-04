0a25935f7f5e9b652da1a237a05394eb3ac72557
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-05 03:57:01 +0530

    feat: add ApiError and ApiResponse utility classes for error handling and response formatting

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express from "express";
-import type{ Express } from "express";
+import type{ Express, NextFunction, Request, Response } from "express";
+import { ApiError } from "./common/utils/apiError";
 
 const app: Express = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+
+    if (err instanceof ApiError) {
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        })
+        return
+    }
+
+    res.status(500).json({
+        success: false,
+        message: 'Internal server error'
+    })
+
+})
 
 export default app;
