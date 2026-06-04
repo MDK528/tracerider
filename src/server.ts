a7ddb2092cbd9e2200918d9667fb6fcbac8aba1b
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-05 03:46:48 +0530

    feat: integrate PostgreSQL with Docker and set up database connection
    - Added a Docker Compose configuration for PostgreSQL service.
    - Created a database configuration file to initialize the Drizzle ORM with PostgreSQL.
    - Updated server initialization to test database connection before starting the server.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import "dotenv/config";
-import app from './app.js';
+import app from "./app.js";
+import { db } from "./common/config/db.js";
+import { sql } from "drizzle-orm/sql/sql";
+
 
 const PORT = process.env.PORT || 5000;
 
+;(async function start () {
+    try {
+        await db.execute(sql`select 1`)
+        
+        app.listen(PORT, ()=>{
+            console.log(`Server is up and running at http://localhost:${PORT}`)
+        })
 
-app.listen(PORT, () => {
-    console.log(`Server is up and running at http://localhost:${PORT}`);
-});
-
+    } catch (error) {
+        console.error('Failed to start server', error)
+        process.exit(1)
+    }
+})()
