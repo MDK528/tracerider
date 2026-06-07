cabc23cd85f379f23170b4538aeb655fd59a761b
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-07 10:16:54 +0530

    feat: add health check endpoint and update auth route integration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import type{ Express, NextFunction, Request, Response } from "express";
 import { ApiError } from "./common/utils/apiError.js";
+import authRoute from "./modules/auth/auth.route.js";
 
 const app: Express = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res)=>{
+    res.json({
+        success: true,
+        message: "healthy"
+    })
+})
+
+app.use("/api/v1/auth", authRoute)
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
