78923423610c11fee1235edefe1c52dc86fd1091
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-09 03:49:53 +0530

    refactor: add pickupLat, pickupLng, dropLat, dropLng, paymentMetho and cancellationReason fields in booking table

--- drizzle/0000_tearful_wraith.sql
+++ drizzle/0000_tearful_wraith.sql
@@ -1,7 +1,29 @@
+CREATE TYPE "public"."gender" AS ENUM('male', 'female');--> statement-breakpoint
+CREATE TYPE "public"."role" AS ENUM('passenger', 'driver', 'admin');--> statement-breakpoint
 CREATE TYPE "public"."cancelled_by" AS ENUM('passenger', 'driver');--> statement-breakpoint
 CREATE TYPE "public"."ride_status" AS ENUM('requested', 'driver_assigned', 'driver_arriving', 'otp_verified', 'in_progress', 'completed', 'cancelled');--> statement-breakpoint
-CREATE TYPE "public"."payment_method" AS ENUM('cash', 'razorpay');--> statement-breakpoint
 CREATE TYPE "public"."payment_status" AS ENUM('pending', 'success', 'failed');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"full_name" varchar(90) NOT NULL,
+	"email" varchar(322) NOT NULL,
+	"is_email_verified" boolean DEFAULT false NOT NULL,
+	"phone" varchar(17) NOT NULL,
+	"gender" "gender",
+	"role" "role" DEFAULT 'passenger' NOT NULL,
+	"address" varchar(340),
+	"avatar_url" varchar,
+	"password" varchar(66),
+	"refresh_token" varchar,
+	"reset_password_token" varchar,
+	"reset_password_expires" timestamp,
+	"email_verification_token" varchar,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_phone_unique" UNIQUE("phone")
+);
+--> statement-breakpoint
 CREATE TABLE "booking" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"passenger_id" uuid NOT NULL,
@@ -9,10 +31,16 @@ CREATE TABLE "booking" (
 	"pickup_city" varchar(100) NOT NULL,
 	"pickup_location" text NOT NULL,
 	"drop_location" text NOT NULL,
+	"pickup_lat" real NOT NULL,
+	"pickup_lng" real NOT NULL,
+	"drop_lat" real NOT NULL,
+	"drop_lng" real NOT NULL,
 	"fare_amount" integer NOT NULL,
+	"payment_method" "payment_method" NOT NULL,
 	"status" "ride_status" DEFAULT 'requested' NOT NULL,
 	"otp" varchar(4),
 	"cancelled_by" "cancelled_by",
+	"cancellation_reason" varchar(200),
 	"confirmed_at" timestamp,
 	"completed_at" timestamp,
 	"cancelled_at" timestamp,
